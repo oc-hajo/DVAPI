6dbb0aea28cddaeab17b421fd312b40be2e5e2cc
Jonas Hagg <jonas.hagg@oneconsult.com>
2024-05-28 15:21:21 +0200

    rename  mongodb docker

--- src/models/db.js
+++ src/models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect('mongodb://mongodb:27017/dvapi', {
+    await mongoose.connect('mongodb://dvapi-mongodb:27017/dvapi', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
